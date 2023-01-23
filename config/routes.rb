cc626e4daec34390e677fc4fe36444b05e63aabc
dailson-igo <dailson.araujo@tce.ap.gov.br>
2023-01-23 07:55:17 -0300

    Arquivos adicionais do sistema

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :usuarios
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: redirect('/admin')
+end
